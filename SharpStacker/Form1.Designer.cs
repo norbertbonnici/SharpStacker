f1317a35ecddaac3e96cf8f96520f6a33815475b
Norbert Bonnici <norbertbonnici@outlook.com>
2018-11-12 20:59:35 +0100

    Initial Commit

---
+++ SharpStacker/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SharpStacker
+{
+    partial class SharpStacker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnStack = new System.Windows.Forms.Button();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.openImage = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(660, 221);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(48, 23);
+            this.btnUp.TabIndex = 0;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.move_image);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(660, 279);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(48, 23);
+            this.btnDown.TabIndex = 0;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.move_image);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(606, 250);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(48, 23);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.move_image);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(714, 250);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(48, 23);
+            this.btnRight.TabIndex = 0;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.move_image);
+            // 
+            // btnStack
+            // 
+            this.btnStack.Location = new System.Drawing.Point(660, 250);
+            this.btnStack.Name = "btnStack";
+            this.btnStack.Size = new System.Drawing.Size(48, 23);
+            this.btnStack.TabIndex = 0;
+            this.btnStack.Text = "Lock";
+            this.btnStack.UseVisualStyleBackColor = true;
+            this.btnStack.Click += new System.EventHandler(this.stack_image);
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(12, 27);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(588, 411);
+            this.imageBox.TabIndex = 1;
+            this.imageBox.TabStop = false;
+            // 
+            // openImage
+            // 
+            this.openImage.FileName = "openImage";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(771, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.open_image);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.stack_image);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // SharpStacker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 450);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnStack);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SharpStacker";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnStack;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.OpenFileDialog openImage;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
+
---
+++ SharpStacker/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SharpStacker
+{
+    public partial class SharpStacker : Form
+    {
+        public SharpStacker()
+        {
+            InitializeComponent();
+        }
+
+        private void move_image(object sender, EventArgs e)
+        {
+            Button direction = (Button)sender;
+
+            switch (direction.Text)
+            {
+                case "Up":
+                    break;
+                case "Down":
+                    break;
+                case "Right":
+                    break;
+                case "Left":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void stack_image(object sender, EventArgs e)
+        {
+
+        }
+
+        private void open_image(object sender, EventArgs e)
+        {
+            openImage = new OpenFileDialog();
+            openImage.ShowDialog();
+            Image stampa = Image.FromFile(openImage.FileName);
+            imageBox.Image = stampa;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
